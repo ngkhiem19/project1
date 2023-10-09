b01f716be79ec33c4f73d4617e649621ce31551b
khiem nguyen <nguyengiakhiem1906@gmail.com>
2023-10-09 17:43:14 +0700

    bt3

---
+++ src/Bt3.java
@@ -0,0 +1,11 @@
+public class Bt3 {
+    public static void main(){
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" ******");
+        System.out.println("********");
+        System.out.println(" ******");
+        System.out.println("  ***");
+        System.out.println("   *");
+    }
+}
